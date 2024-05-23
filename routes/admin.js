b8c9d3bcb7c8bf8c1986004cb09b3deb11df96a1
SidhardhCR <sidhardhcr007@gmail.com>
2024-05-23 21:23:12 +0530

    add-product page added

--- routes/admin.js
+++ routes/admin.js
@@ -94,4 +94,14 @@ router.get('/', function (req, res, next) {
   res.render('admin/view-products', { products, admin: true })
 });
 
+router.get('/add-product', (req, res) => {
+  res.render('admin/add-product')
+})
+
+router.post('/add-product', (req, res) => {
+  console.log(req.body)
+  console.log(req.files.image)
+  res.render('admin/add-product')
+})
+
 module.exports = router;
